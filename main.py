5bd590d4597854042c2f4670ebb5e0a24b038052
Rus <islam06.95@mail.ru>
2023-10-07 17:45:47 +0300

    add

---
+++ main.py
@@ -0,0 +1,33 @@
+import json
+from pprint import pprint
+import requests
+from bs4 import BeautifulSoup
+
+vacancy1 = ['Django', 'Flask']
+headers = {
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/58.0.3029.110 ',
+}
+url = "https://spb.hh.ru/search/vacancy?text=python&area=1&area=2"
+response = requests.get(url, headers=headers)
+
+vacancy_list = []
+vacancy_info = {}
+soup = BeautifulSoup(response.text, 'html.parser')
+vacancies = soup.find_all(class_="vacancy-serp-item__layout")
+for vacancy in vacancies:
+    name = vacancy.find("a", class_="serp-item__title").text
+    company = vacancy.find("a", class_="bloko-link bloko-link_kind-tertiary").text
+    link = vacancy.find("a", class_="serp-item__title")['href']
+    salary = vacancy.find("span", class_="bloko-header-section-2")
+    city = vacancy.find("div", {"data-qa": "vacancy-serp__vacancy-address", "class": "bloko-text"}).text
+    vacancy_info = {
+        'link': link,
+        'company': company,
+        'city': city,
+        'salary': salary,
+        'name' : name
+                }
+    vacancy_list.append(vacancy_info)
+# pprint(vacancy_list)
+with open('vacancies.json', 'w', encoding='utf-8') as file:
+    json.dump(vacancy_list, file, ensure_ascii=False, indent=4)
